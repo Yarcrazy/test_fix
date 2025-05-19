35ab3af6889cd33de94ccd1286177cdf1e164b69
itatarinov <yarcrazy2323@gmail.com>
2025-05-19 19:37:31 +0300

    json format

--- app/config/web.php
+++ app/config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'tG4C0yaGKyCyH6DeMGelg5OuAUmH_xKz',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
